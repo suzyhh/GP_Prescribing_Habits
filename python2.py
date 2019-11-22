0bca7bbf86ae7b084ddb60790ee82360565dddca
suzy <suzyhocking@gmail.com>
2019-11-22 12:23:14 +0000

    added sort

--- python2.py
+++ python2.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[87]:
+# In[335]:
 
 
 import csv
@@ -10,14 +10,14 @@ import matplotlib.pyplot as pyplot
 get_ipython().run_line_magic('matplotlib', 'inline')
 
 
-# In[88]:
+# In[336]:
 
 
 def readfile(file):
     df=pd.read_csv(file)
 
 
-# In[89]:
+# In[337]:
 
 
 df=pd.read_csv("items for penicillins per 1,000 patients on list.csv")
@@ -29,35 +29,35 @@ df_patients=df[df["total_list_size"]!=0].copy()
 naGPs=df[df["total_list_size"]==0].copy()
 
 
-# In[90]:
+# In[338]:
 
 
-df_patients['normalisation']=df_patients['y_items'] / df_patients['total_list_size']
+df_patients['normalisation']=(df_patients['y_items'] / df_patients['total_list_size'])*1000
 #df_timeseries=df_patients.copy()
 #df_timeseries['date'] = pd.to_datetime(df_patients.date, format = '%d/%m/%Y')
 
 
-# In[106]:
+# In[339]:
 
 
 df_by_date_mean=df_patients.groupby("date")["normalisation"].mean()
 df_by_date_std=df_patients.groupby("date")["normalisation"].std()
 
 
-# In[107]:
+# In[340]:
 
 
 df_by_date_mean  = df_by_date_mean.rename('mean')
 
 
-# In[109]:
+# In[341]:
 
 
 mean_dict = df_by_date_mean.to_dict() 
 std_dict = df_by_date_std.to_dict()
 
 
-# In[110]:
+# In[342]:
 
 
 df_patients['std'] = df_patients['date'].map(std_dict)
@@ -65,14 +65,14 @@ df_patients['mean'] = df_patients['date'].map(mean_dict)
 df_patients['z'] = (df_patients['normalisation']-df_patients['mean'])/df_patients['std']
 
 
-# In[112]:
+# In[343]:
 
 
 outliers1 = df_patients[(df_patients['z'] >= 2)].copy() 
 outliers2 = df_patients[(df_patients['z'] <= -2)].copy()
 
 
-# In[113]:
+# In[344]:
 
 
 #need to convert to timeseries
@@ -82,7 +82,7 @@ outliers1['date'] = pd.to_datetime(df_patients.date, format = '%d/%m/%Y')
 outliers2['date'] = pd.to_datetime(df_patients.date, format = '%d/%m/%Y')
 
 
-# In[99]:
+# In[345]:
 
 
 df_by_date_std=df_patients.groupby("date")["normalisation"].std()
@@ -90,51 +90,86 @@ meanstd_plus=df_by_date_mean + df_by_date_std
 meanstd_minus=df_by_date_mean - df_by_date_std
 
 
-# In[128]:
+# In[346]:
 
 
 summary_data = pd.concat([df_by_date_mean,meanstd_minus, meanstd_plus], axis=1)
 
 
-# In[129]:
+# In[347]:
 
 
 summary_data=summary_data.rename(columns={0:"mean - std",1:"mean + std"})
 
 
-# In[133]:
+# In[348]:
 
 
 summary_data.index=pd.to_datetime(summary_data.index,format='%d/%m/%Y')
 
 
-# In[149]:
+# In[349]:
 
 
 #outliers1.plot(kind="scatter",x="date",y='z',color='red')
 
 
-# In[182]:
+# In[350]:
 
 
-
-pyplot.plot_date(summary_data.index,summary_data["mean"],c="blue")
-pyplot.plot_date(summary_data.index,summary_data["mean + std"],c="green")
-pyplot.plot_date(summary_data.index,summary_data["mean - std"],c="green")
-pyplot.plot_date(outliers1["date"],outliers1["normalisation"],c="red")
-pyplot.plot_date(outliers2["date"],outliers2["normalisation"],c="red")
+sort=summary_data.sort_index()
 
 
-# In[176]:
+# In[351]:
 
 
-pyplot.plot(summary_data)
+pyplot.plot(sort)
+pyplot.plot_date(outliers1["date"],outliers1["normalisation"],c="red",markersize=3)
+pyplot.plot_date(outliers2["date"],outliers2["normalisation"],c="red",markersize=3)
 
 
-# In[167]:
+# In[352]:
 
 
-summary_data
+outliers1[outliers1['normalisation']==outliers1['normalisation'].max()]
+
+
+# In[353]:
+
+
+practices=df_patients.groupby("name")["normalisation"].mean()
+
+
+# In[330]:
+
+
+practices
+
+
+# In[310]:
+
+
+practices_mean=high_practices.mean()
+practices_std=practices.std()
+
+
+# In[313]:
+
+
+practices_mean
+practices_std
+
+
+# In[333]:
+
+
+prac_outliers= practices>practices_mean
+
+
+# In[334]:
+
+
+prac_outliers
 
 
 # In[ ]:
