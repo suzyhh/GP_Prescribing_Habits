7f9c9a149a1af87251b95b89a7f85d5ba472ec9b
Rachel Muir <rachel.muir@gosh.nhs.uk>
2020-01-14 16:50:21 +0000

    reformatting

--- prescribing_habits.py
+++ prescribing_habits.py
@@ -1,14 +1,10 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[335]:
-
 import pandas as pd
 import matplotlib.pyplot as pyplot
 
 # get_ipython().run_line_magic('matplotlib', 'inline')
-
-# In[337]:
 
 # ask for a file to analyse and raise an exception if the file is not found in the working directory
 # for testing, the filename is "items for penicillins per 1,000 patients on list.csv"
@@ -24,29 +20,19 @@ prescriptions = prescriptions_raw[prescriptions_raw["total_list_size"] != 0].cop
 # assign GPs with no patients to naGPs
 GP_no_patients = prescriptions_raw[prescriptions_raw["total_list_size"] == 0].copy()
 
-# In[338]:
-
 # normalise the number of items per 1000 patients registered at the practice
 prescriptions['normalisation'] = (prescriptions['y_items'] / prescriptions['total_list_size']) * 1000
-
-# In[339]:
 
 # find mean and standard deviation for z-score calculation
 mean_prescriptions_by_date = prescriptions.groupby("date")["normalisation"].mean()
 std_prescriptions_by_date = prescriptions.groupby("date")["normalisation"].std()
 
-# In[340]:
-
 # rename column
 mean_prescriptions_by_date = mean_prescriptions_by_date.rename('mean')
-
-# In[341]:
 
 # create dictionaries for mean and standard deviation so we can refer to values
 mean_prescriptions_dict = mean_prescriptions_by_date.to_dict()
 std_prescriptions_dict = std_prescriptions_by_date.to_dict()
-
-# In[342]:
 
 # referring the dataframe of patients to the associated mean and std based on date
 prescriptions['std'] = prescriptions['date'].map(std_prescriptions_dict)
@@ -54,15 +40,9 @@ prescriptions['mean'] = prescriptions['date'].map(mean_prescriptions_dict)
 # calculating the z score
 prescriptions['z'] = (prescriptions['normalisation'] - prescriptions['mean']) / prescriptions['std']
 
-# In[343]:
-
-
 # finding outliers per month that are more than 2x the standard devations away from the mean
 outliers_over = prescriptions[(prescriptions['z'] >= 2)].copy()
 outliers_under = prescriptions[(prescriptions['z'] <= -2)].copy()
-
-# In[344]:
-
 
 # convert to timeseries
 # df2['date'] = pd.to_datetime(df2.date, format = '%d/%m/%Y')
@@ -70,33 +50,20 @@ outliers_under = prescriptions[(prescriptions['z'] <= -2)].copy()
 outliers_over['date'] = pd.to_datetime(prescriptions.date, format='%d/%m/%Y')
 outliers_under['date'] = pd.to_datetime(prescriptions.date, format='%d/%m/%Y')
 
-# In[364]:
 meanstd_plus = mean_prescriptions_by_date + std_prescriptions_by_date
 meanstd_minus = mean_prescriptions_by_date - std_prescriptions_by_date
 
 # create dataframe with the mean and the standard devations
 prescription_summary = pd.concat([mean_prescriptions_by_date, meanstd_minus, meanstd_plus], axis=1)
 
-# In[365]:
-
-
 # rename columns with meaningful headings
 prescription_summary = prescription_summary.rename(columns={0: "mean - std", 1: "mean + std"})
-
-# In[366]:
-
 
 # convert date index to timeseries
 prescription_summary.index = pd.to_datetime(prescription_summary.index, format='%d/%m/%Y')
 
-# In[368]:
-
-
 # sort the timeseries
 sorted_summary = prescription_summary.sort_index()
-
-# In[369]:
-
 
 # plotting the mean and standard deviation from summary data and then overlay with the outliers
 [mean, plus, minus] = pyplot.plot(sorted_summary)
@@ -107,9 +74,6 @@ pyplot.tick_params(axis='x', rotation=45)
 pyplot.ylabel('Prescriptions (per 1000 patients)')
 pyplot.legend([mean, plus, minus, outlier], ["mean", "mean-1 stdev", "mean+1 stdev", "outliers"])
 pyplot.show()
-
-# In[370]:
-
 
 # find which GP in which month prescribed the most items
 highest_prescription = outliers_over[outliers_over['normalisation'] == outliers_over['normalisation'].max()]
@@ -155,8 +119,6 @@ pyplot.legend([mean, plus, minus, highest], ["mean", "mean-1 stdev", "mean+1 std
 pyplot.show()
 
 
-# In[371]:
-
 def plot_a_practice(GP_practice):
     """Plotting the prescribing of a single GP surgery compared to the mean/std of all surgerys
     A match will be made from an incomplete input and is case insensitive (e.g. "hawthorn" will match to "HAWTHORN MC")
@@ -173,6 +135,7 @@ def plot_a_practice(GP_practice):
     pyplot.legend([mean, plus, minus, highest], ["mean", "mean-1 stdev", "mean+1 stdev", practice_details.iloc[0, 2]])
     pyplot.show()
 
+
 def check_gp_lookup(GP_practice):
     """Check valid GP practice is found"""
     practice_details = prescriptions[prescriptions['name'].str.match(pat=".*{}.*".format(GP_input_file), case=False)]
@@ -185,6 +148,7 @@ def check_gp_lookup(GP_practice):
             uniq_lst.append(surgery)
     if len(uniq_lst) > 1:
         raise LookupError
+
 
 GP_input_file = input("Enter the GP practice: ")
 
@@ -199,5 +163,3 @@ while True:
     except NameError:
         print("GP surgery does not exist, please enter a valid GP")
         GP_input_file = input("Enter the GP practice: ")
-
-
